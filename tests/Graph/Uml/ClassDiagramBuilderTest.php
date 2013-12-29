45592ae90edc60d356d1346cab83deeb7e785b05
Clemens Tolboom <clemens@build2be.com>
2013-12-29 16:00:17 +0100

    Restructure test setup.

--- tests/Graph/Uml/ClassDiagramBuilderTest.php
+++ tests/Graph/Uml/ClassDiagramBuilderTest.php
@@ -2,15 +2,15 @@
 
 class ClassDiagramBuilderTest extends TestCase
 {
-    private $graph;
-    private $builder;
 
     public function setup()
     {
-        $this->graph = new Fhaculty\Graph\Graph();
-        $this->builder = new Fhaculty\Graph\Uml\ClassDiagramBuilder($this->graph);
+        parent::setup();
     }
 
+    /**
+     * This does not fail as we build bottom to top.
+     */
     public function testParentChildSuccess()
     {
         $this->builder->createVertexClass('Fhaculty\Graph\Edge\Base');
@@ -23,6 +23,10 @@ class ClassDiagramBuilderTest extends TestCase
      * Adding a sub-class (Edge\Directed) also automatically adds its parent (Edge\Base).
      * So trying to add the parent (Edge\Base) again fails because of a duplicate class name.
      *
+     * This test will become obsolete when
+     * we fix https://github.com/clue/graph/issues/85
+     *
+     * @todo Remove exception when ready.
      * @expectedException RuntimeException
      */
     public function testChildParentFail()
