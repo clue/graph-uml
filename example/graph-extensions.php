f4566390bd1646c4659c3ce750a027614ba400b8
Christian Lück <christian@lueck.tv>
2014-02-24 22:12:39 +0100

    Randomly pick only two extensions to display

--- example/graph-extensions.php
+++ example/graph-extensions.php
@@ -9,6 +9,11 @@ $graph = new Fhaculty\Graph\Graph();
 $builder = new Fhaculty\Graph\Uml\ClassDiagramBuilder($graph);
 
 $all = get_loaded_extensions(false);
+
+// adding all extensions will result in a huge graph, so just pick 2 random ones
+shuffle($all);
+$all = array_slice($all, 0, 2);
+
 // $all = array('PDO');
 foreach ($all as $one) {
     if (!$graph->hasVertex($one)) {
